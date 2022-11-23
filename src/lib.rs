5d57506bee40679834cd85a09f03256b69f1b309
Louis Wyborn <louiswyborn@gmail.com>
2022-11-23 17:37:56 +0100

    Make tls and encryption modules public

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ pub mod protocol;
 mod server;
 pub mod stream;
 #[cfg(any(feature = "native-tls", feature = "__rustls-tls"))]
-mod tls;
+pub mod tls;
 pub mod util;
 
 const READ_BUFFER_CHUNK_SIZE: usize = 4096;
